5ed744e697ce52079f4780eb0906533c809f767a
Brad Schmitt <bschmitt@newrelic.com>
2022-06-20 18:02:02 -0500

    update memory alert

--- modules/newrelic_k8s_alerts/main.tf
+++ modules/newrelic_k8s_alerts/main.tf
@@ -115,7 +115,7 @@ resource "newrelic_nrql_alert_condition" "container_memory_utilization" {
   slide_by                       = 30
 
   nrql {
-    query = "from K8sContainerSample select max(memoryWorkingSetUtilization) where clusterName = '${local.cluster_name}' facet containerName, podName"
+    query = "from K8sContainerSample select max(memoryWorkingSetUtilization) where clusterName = '${local.cluster_name}' and status not in ('Terminated','Completed') facet containerName, podName"
   }
 
   critical {
@@ -351,4 +351,3 @@ resource "newrelic_nrql_alert_condition" "etcd_has_leader" {
     threshold_occurrences = "ALL"
   }
 }
-
